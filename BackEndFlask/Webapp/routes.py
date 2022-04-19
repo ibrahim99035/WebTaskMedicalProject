91bb8baaa1a02ff880dd7a6cb33113c882556192
Ibrahim mohamed <ebrahimaboeita990@gmail.com>
2022-04-19 05:30:19 +0200

    svaing initial task changes

--- BackEndFlask/Webapp/routes.py
+++ BackEndFlask/Webapp/routes.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
-from Webapp import app
+from Webapp import app, db, bcrypt
 from Webapp.auth import RegistrationForm, LoginForm
 from Webapp.models import User, Res
 
@@ -115,13 +115,20 @@ def about():
 def signup():
     form = RegistrationForm()
     if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
 
     return render_template('signup.html', title = 'Sign up', form = form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = LoginForm()
     if form.validate_on_submit():
         if form.email.data == 'admin@blog.com' and form.password.data == 'password':
