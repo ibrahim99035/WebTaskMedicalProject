4338ca55c0f08ffad1506da4e998bf68cf4e6c0b
Ibrahim mohamed <ebrahimaboeita990@gmail.com>
2022-04-21 06:27:45 +0200

    the funcionality of diabetes prediction system

--- BackEndFlask/Webapp/diabetes.py
+++ BackEndFlask/Webapp/diabetes.py
@@ -8,45 +8,29 @@ class DiabetesForm(FlaskForm):
     Hours_After_Eating = FloatField('2-3 Hours After Eating', validators=[DataRequired()])
     submit = SubmitField('Confirm')
 
+    #patient cases with diabetes
+    case1 = 'The patient is normal'
+    case2 = 'The patient is Impaired Glucose'
+    case3 = 'The patient is Diabetic'
+    case4 = 'The patient is in a diabetic coma'
+    
+    diabetesResult = ''
 
+    def checkTheCase(self, Fasting, After_Eating, Hours_After_Eating):
+        #diabetes tests boolean variables
+        test1 = 80 <= Fasting <= 100 and 170 <= After_Eating <= 200 and 120 <= Hours_After_Eating <= 140
+        test2 = 101 <= Fasting <= 125 and 190 <= After_Eating <= 230 and 140 <= Hours_After_Eating <= 160
+        test3 = Fasting >=126 and 220 <= After_Eating <= 300 and  Hours_After_Eating >= 200
+        test4 = Fasting <80
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-Fasting = float(input("enter value of Fasting :"))
-After_Eating =float(input("enter value of After_Eating :"))
-Hours_After_Eating = float(input("enter value of 2-3Hours_After_Eating :"))
-
-#patient cases with diabetes
-case1 = 'The patient is normal'
-case2 = 'The patient is Impaired Glucose'
-case3 = 'The patient is Diabetic'
-case4 = 'The patient is in a diabetic coma'
-
-#diabetes tests boolean variables
-test1 = 80 <= Fasting <= 100 and 170 <= After_Eating <= 200 and 120 <= Hours_After_Eating <= 140
-test2 = 101 <= Fasting <= 125 and 190 <= After_Eating <= 230 and 140 <= Hours_After_Eating <= 160
-test3 = Fasting >=126 and 220 <= After_Eating <= 300 and  Hours_After_Eating >= 200
-test4 = Fasting <80
-
-#the functionality 
-diabetesResult = ''
-if test1:
-    diabetesResult = case1
-elif test2:
-    diabetesResult = case2
-elif test3:
-    diabetesResult = case3
-elif test4:
-    diabetesResult = case4
-
+        #the functionality 
+        if test1:
+            diabetesResult = self.case1
+        elif test2:
+            diabetesResult = self.case2
+        elif test3:
+            diabetesResult = self.case3
+        elif test4:
+            diabetesResult = self.case4
+        
+        return diabetesResult
