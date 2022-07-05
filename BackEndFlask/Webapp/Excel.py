d0300465f73cdecd0b45bc8b9d2926c3e80b5a11
Ibrahim mohamed <ebrahimaboeita990@gmail.com>
2022-07-05 23:08:09 +0200

    excel saving first try

--- BackEndFlask/Webapp/Excel.py
+++ BackEndFlask/Webapp/Excel.py
@@ -2,21 +2,49 @@ import xlrd
 from Webapp.models import Patients
 from flask_login import current_user
 from Webapp import db
-class Excelentry:
-    WorkBook = xlrd.open_workbook('.\\Excel\\Excel.xlsx')
-    Sheet = WorkBook.sheet_by_index(0)
-    limitRows = Sheet.nrows
-    resultRecord = Patients()
+from flask_wtf import FlaskForm
+from wtforms import FloatField, SubmitField, StringField
+from wtforms.validators import DataRequired, Length
+from flask_wtf.file import FileField, FileAllowed
+class Excelentry(FlaskForm):
+    TheFile = FileField('Upload Excel File', validators=[FileAllowed(['xlsx', 'xls'])])
+    submit = SubmitField('Upload')
+    
     def addRecord(self, ExcelPath):
-        for i in range(1, self.limitRows):
-            self.resultRecord.name = self.Sheet.cell_value(i, 0)
-            self.resultRecord.age = self.Sheet.cell_value(i, 1)
-            self.resultRecord.nationalID = self.Sheet.cell_value(i, 2)
-            self.resultRecord.date_entered = self.Sheet.cell_value(i, 3)
-            self.resultRecord.diabetes = self.Sheet.cell_value(i, 4)
-            self.resultRecord.blood_presure = self.Sheet.cell_value(i, 5)
-            self.resultRecord.covid_19 = self.Sheet.cell_value(i, 6)
-            #-----------------------------------------------------
-            db.add(self.resultRecord)
-            db.commit()
+        WorkBook = xlrd.open_workbook(ExcelPath)
+        Sheet = WorkBook.sheet_by_index(0)
+        limitRows = Sheet.nrows
+        #-------------------------------------------------
+        #add default values to the dtabase
+        name = 'default'
+        age = 0
+        nationalID = 'default'
+        diabetes = 'default'
+        blood_presure = 'default'
+        covid_19 = 'default'
+        #-------------------------------------------------
+        for i in range(1, limitRows):
+            name = Sheet.cell_value(i, 0)
+            age = Sheet.cell_value(i, 1)
+            nationalID = Sheet.cell_value(i, 2)
+            diabetes = Sheet.cell_value(i, 3)
+            blood_presure = Sheet.cell_value(i, 4)
+            covid_19 = Sheet.cell_value(i, 5)
+        
+        DataList = []
+        if name != 'default':
+            DataList.append(name)
+        if age > 0:
+            DataList.append(age)
+        if nationalID != 'default':
+            DataList.append(nationalID)
+        if diabetes != 'default':
+            DataList.append(diabetes)
+        if blood_presure != 'default':
+            DataList.append(blood_presure)
+        if covid_19 != 'default':
+            DataList.append(covid_19)
+        
+        #-----------------------------------------------------
+        return DataList
         
